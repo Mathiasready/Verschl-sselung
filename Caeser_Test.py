11d7c7b0e67df397240ff3ae445e04a07a979bfd
permat07 <permat07@rg16.at>
2023-09-25 16:17:52 +0200

    added test case

---
+++ Caeser_Test.py
@@ -0,0 +1,8 @@
+import pytest
+import Caesar
+
+def test_encrypt_basic():
+    assert Caesar.encrypt("abc",1) == "bcd"
+    assert Caesar.encrypt("abc",1) == "bcd"
+    assert Caesar.encrypt("a",6) == "f"
+    assert Caesar.encrypt("ac",1) == "bd"
