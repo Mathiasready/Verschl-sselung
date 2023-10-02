4226ab2cacc15493df230183e95cca58d6d50040
permat07 <permat07@rg16.at>
2023-10-02 16:01:05 +0200

    corrected some errors added random msg test case

--- Caesar.py
+++ Caesar.py
@@ -1,14 +1,24 @@
-abc=["a","b","c","d","e","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","x","y","z"]
+abc = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u",
+       "v", "w", "x", "y", "z"]
+
+
 def encrypt(msg_clear: str, key: int):
-    msg_hidden=""
-    for c in msg_clear:
+    msg_hidden = ""
+    for c in msg_clear.lower():
         try:
-            i=abc.index(c)
-            new_i=i+key
-            new_c=abc[new_i]
+            i = abc.index(c)
+            new_i = (i + key) % 26
+            new_c = abc[new_i]
         except ValueError:
-            new_c = c
-        msg_hidden+=new_c
+            if c.isnumeric():
+                i = int(c)
+                new_i = (i + key) % 10
+                new_c = str(new_i)
+            else:
+                new_c = c
+        msg_hidden += new_c
     return msg_hidden
 
-print(encrypt("abc",3))
+
+def decrypt(hidden_msg: str, key: int):
+    return encrypt(hidden_msg, -key)
--- Caeser_Test.py
+++ Caeser_Test.py
@@ -1,10 +1,31 @@
 import pytest
 import Caesar
+import random
+import string
+
+
+def get_random_string(length):
+    # choose from all lowercase letter
+    letters = string.ascii_letters
+    result_str = ''.join(random.choice(letters) for i in range(length))
+    return result_str
+
+
+def test_encrypt_random():
+    for i in range(1000):
+        clear = get_random_string(random.randint(0, 100))
+        key = random.randint(0, 100)
+        hidden = Caesar.encrypt(clear, key)
+        decrypted = Caesar.decrypt(hidden, key)
+        assert clear.lower() == decrypted
+
 
 def test_encrypt_basic():
-    assert Caesar.encrypt("abc",1) == "bcd"
-    assert Caesar.encrypt("abc",1) == "bcd"
-    assert Caesar.encrypt("a",6) == "f"
-    assert Caesar.encrypt("ac",1) == "bd"
-    assert Caesar.encrypt("9:00",1) == "9:00"
-
+    assert Caesar.encrypt("abc", 1) == "bcd"
+    assert Caesar.encrypt("a", 3) == "d"
+    assert Caesar.encrypt("", 1) == ""
+    assert Caesar.encrypt("hallo", 2) == "jcnnq"
+    assert Caesar.encrypt("Hi", 2) == "jk"
+    assert Caesar.encrypt("Hi!", 2) == "jk!"
+    assert Caesar.encrypt("9:00", 2) == "1:22"
+    assert Caesar.encrypt("xyz", 2) == "zab"
