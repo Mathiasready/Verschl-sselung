af3b625ea68780bfefcea22552f26482ed7c3b83
permat07 <permat07@rg16.at>
2023-10-09 15:46:13 +0200

    is able to encrypt, decrypt and brute force

--- Casesar_cli.py
+++ Casesar_cli.py
@@ -1,10 +1,39 @@
 import Caesar
+
 print("Willkommen zum Verschlüsselungsverfahren")
-print("-"*60)
-while True:
-    msg_clear=input("Was willst du verschlüsseln? (Enter um zum beenden)")
-    if msg_clear =="":
-        break
-    key=int(input("Welches Passwort möchtest du verwenden?"))
-    msg_hidden=Caesar.encrypt(msg_clear, key)
+print("-" * 60)
+
+
+def do_encrypt():
+    msg_clear = input("Was willst du verschlüsseln?")
+    key = int(input("Welches Passwort möchtest du verwenden?"))
+    msg_hidden = Caesar.encrypt(msg_clear, key)
     print(msg_hidden)
+
+
+def do_decrypt():
+    msg_clear = input("Was willst du entschlüsseln?")
+    d_key = int(input("Welches Passwort möchtest du verwenden?"))
+    msg_hidden = Caesar.decrypt(msg_clear, d_key)
+    print(msg_hidden)
+
+
+def do_bruteforce():
+    msg_clear = input("Was willst du entschlüsseln?")
+    for key in range(26):
+        msg_hidden = Caesar.decrypt(msg_clear, key)
+        print(f"{key:02d}: {msg_hidden}")
+
+
+while True:
+    selection = input("was möchtest du tun? (e)ncrypt, (d)ecrypt, (b)rute, force e(x)it")
+    if selection == "e":
+        do_encrypt()
+    elif selection == "d":
+        do_decrypt()
+    elif selection == "b":
+        do_bruteforce()
+    elif selection == "x":
+        break
+    else:
+        print(f"Funktion {selection} konnte nicht gefunden werden")
